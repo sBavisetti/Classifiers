a1406ad0e7b8f212ed99290fc13dfb48894f1a92
KingAsura <srinivasmahavee.bavisetti@mavs.uta.edu>
2016-10-31 15:16:00 -0500

    Add files via upload

---
+++ main.m
@@ -0,0 +1,22 @@
+close all;
+clear all;
+% Reading Training data from text files;
+train_data = csvread('DataMiningProject1/ATNT50/trainDataXY.txt');
+test_data =  csvread('DataMiningProject1/ATNT50/testDataXY.txt');
+
+%transposing both training data and testing data  to put them in proper
+%formats
+train_data = train_data';
+test_data = test_data';
+
+%Knn Classifier:
+knn_result = Knn(train_data,test_data,3)
+knn_accuracy = Accuracy(knn_result)
+
+% Centroid Classifier
+centroid_result = centroid(train_data,test_data)
+centroid_accuracy = Accuracy(centroid_result)
+
+% Linear Regression Classifer
+linear_regression_result = LinearRegression(train_data,test_data)
+linear_regression_accuracy = Accuracy(linear_regression_result)
